de57a1821a6781f2202ad2d9a76c08314176f670
Safwan Abdulaghani <sfwanabdualghni@gmail.com>
2022-03-03 12:56:33 +0300

    Make Group Nullable

--- CleanStructure/GroupEvent.cs
+++ CleanStructure/GroupEvent.cs
@@ -2,11 +2,11 @@
 {
     public abstract record GroupEvent;
 
-    public sealed record InsertGroup(Group Group) : GroupEvent;
+    public sealed record InsertGroup(Group? Group) : GroupEvent;
 
-    public sealed record UpdateGroup(Group Group) : GroupEvent;
+    public sealed record UpdateGroup(Group? Group) : GroupEvent;
 
-    public sealed record DeleteGroup(Group Group) : GroupEvent;
+    public sealed record DeleteGroup(Group? Group) : GroupEvent;
     
     public sealed record FindGroup(string Name) : GroupEvent;
 }
